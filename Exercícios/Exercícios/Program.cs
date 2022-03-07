3029dfab622d68a9e895904aadbd201bde9f24a5
Amanda Funke <amanda.funke@cppro.com.br>
2022-03-07 11:27:49 -0300

    teste

---
+++ Exercícios/Exercícios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercícios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
