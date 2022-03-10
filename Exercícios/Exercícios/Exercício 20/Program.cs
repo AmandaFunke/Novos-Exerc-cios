40eecdd5075cccc693631654c0f993f4e0bcacf6
Amanda Funke <amanda.funke@cppro.com.br>
2022-03-10 15:28:22 -0300

    add 20 e 21

---
+++ Exercícios/Exercícios/Exercício 20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Data.Common;
+
+namespace Exercício_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a program that reads a text file and displays the number of words. */
+
+            var filePath = @"C:\Users\amand\Documents\Exercícios Programação\README.md";
+
+            if (File.Exists(filePath))
+            {
+                var splitedContent = File.ReadAllText(filePath).Split(' ');
+
+                Console.WriteLine(splitedContent.Length);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercícios/Exercícios/Exercício 21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Exercício_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a program that reads a text file and displays the longest word in the file. */
+
+            var filePath = @"C:\Users\amand\Documents\Exercícios Programação\README.md";
+            
+            if (File.Exists(filePath))
+            {
+                var splitedContent = File.ReadAllText(filePath).Split(' ');
+
+                var wordsLength = new List<int>();
+
+                foreach (var word in splitedContent)
+                    wordsLength.Add(word.Length);
+
+                wordsLength.Sort();
+                
+                var IndexOfLongestWord = wordsLength.Count();
+                var longestWord = wordsLength[IndexOfLongestWord];
+
+                foreach (var word in splitedContent)
+                {
+                    if (word.Length == longestWord)
+                        Console.WriteLine(word);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
